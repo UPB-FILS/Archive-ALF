087a15237f9621c7ccb03f07f457ddb33822f9b1
Dragos STOICA <dragos_constantin_stoica@yahoo.com>
2015-03-29 20:25:49 +0300

    Update las.js

--- las.js
+++ las.js
@@ -1,9 +1,14 @@
 function lookandsay(str) {
     return str.replace(/(.)\1*/g, function(seq, p1){return seq.length.toString() + p1})
 }
- 
-var num = "1";
+
+function sayandlook(str) {
+    return str.replace(/(.)\1*/g, function(seq, p1){return p1 + seq.length.toString()})
+}
+
+var num = "0";
 for (var i = 10; i > 0; i--) {
-    alert(num);
+    console.log(num);
     num = lookandsay(num);
+    //num = sayandlook(num);
 }
